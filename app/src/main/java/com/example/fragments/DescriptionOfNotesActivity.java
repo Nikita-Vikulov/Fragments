0c84cc1f35e995a844955d7bc8e34dc20b4a6016
Nikta <n.vikulov92@yandex.ru>
2021-04-12 12:50:05 +0300

    HomeWork6

--- app/src/main/java/com/example/fragments/DescriptionOfNotesActivity.java
+++ app/src/main/java/com/example/fragments/DescriptionOfNotesActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CoatOfNotesActivity extends AppCompatActivity {
+public class DescriptionOfNotesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coat_of_notes);
+        setContentView(R.layout.activity_description_of_notes);
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             // Если устройство перевернули в альбомную ориентацию, то надо эту activity закрыть
@@ -20,7 +20,7 @@ public class CoatOfNotesActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             // Если эта activity запускается первый раз (с каждым новым гербом первый раз),
             // то перенаправим параметр фрагменту
-            CoatOfNotesFragment details = new CoatOfNotesFragment();
+            DescriptionOfNotesFragment details = new DescriptionOfNotesFragment();
             details.setArguments(getIntent().getExtras());
             // Добавим фрагмент на activity
             getSupportFragmentManager()
